e25319509381f876b68e1d781d0be6fc8027dfdf
joshchif3 <pantherchif3@gmail.com>
2023-12-14 14:45:27 -0800

    4 updatess

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- *  * main - this is the main function
- *   *
- *    * Return: 0 when successfull
- *     */
+ *main - this is the main function
+ * Return: 0 (success)
+ */
 
 int main(void)
 {
-	        puts("\"Programming is like building a multilingual puzzle\n");
+	        puts("\"Programming is like building a multilingual puzzle");
 		        return (0);
 }
