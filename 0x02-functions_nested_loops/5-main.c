3c89a7ce0d2b6dd2a67aaf093f72ad941c9700ba
joshchif3 <pantherchif3@gmail.com>
2023-12-20 11:41:27 -0800

    5changs

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,44 +1,10 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
  * main - check the code.
  *
  * Return: Always 0.
- */ 
-int main(void)  { int r;
-	 r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
-
-
-=======
-#include "main.h"
-
-/**
-* main - check the code.
-*
-* Return: Always 0.
-*/
+ */
 int main(void)
 {
 int r;
@@ -65,4 +31,3 @@ _putchar(r + '0');
 _putchar('\n');
 return (0);
 }
->>>>>>> 6bab29a208bc5cec518596b13f72f576ce8fd586
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -7,39 +6,22 @@
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
- */ 
-	int print_sign(int n)  { if (n > 0)
-		{ _putchar('+');
-		  return (1);
-		  } else if (n == 0) } _putchar(48); return (0); } else
-		  if (n < 0)  { _putchar('-'); }
-		  return (-1); }
-
-		  
-=======
-#include "main.h"
-
-/**
-* print_sign - prints the sign of a number
-* @n: the int to check
-* Return: 1 and prints + if n is greater than zero
-* 0 and prints 0 if n is zero
-* -1 and prints - if n is less than zero
-*/
+ */
 int print_sign(int n)
 {
-if (n > 0)
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+}
+else if (n == 0)
 {
-_putchar('+');
-return (1);
-} else if (n == 0)
+		_putchar(48);
+		return (0);
+} 
+else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
 }
-_putchar(48);
-return (0);
-} else if (n < 0)
-{
-_putchar('-');
-}
-return (-1);
-}
->>>>>>> 6bab29a208bc5cec518596b13f72f576ce8fd586
