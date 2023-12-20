58f5a48a5b4158c330146ef2650fb83ef69e033c
joshchif3 <pantherchif3@gmail.com>
2023-12-19 18:46:59 -0800

    changes to 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -10,16 +9,3 @@ int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
-=======
-#include "main.h"
-
-/**
-* _isalpha - checks for alphabetic character
-* @c: the character to be checked
-* Return: 1 if c is a letter, 0 otherwise
-*/
-int _isalpha(int c)
-{
-return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
->>>>>>> 6bab29a208bc5cec518596b13f72f576ce8fd586
