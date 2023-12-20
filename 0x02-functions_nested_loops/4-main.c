0123cb236d3ae0d4ee99d1644e4bdd4242613512
Joshua Chifura <pantherchif3@gmail.com>
2023-12-20 03:51:06 +0200

    4-main.c

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * main - check the code.
@@ -14,7 +14,7 @@ _putchar(r + '0');
 r = _islower('o');
 _putchar(r + '0');
 r = _islower(108);
-_putchar(r + '0');
+	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
 }
