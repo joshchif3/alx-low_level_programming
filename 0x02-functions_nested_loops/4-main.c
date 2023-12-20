d55790424dde36e2144dadf0d3333a7b57281aeb
Joshua Chifura <pantherchif3@gmail.com>
2023-12-20 03:57:02 +0200

    4-main.c

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
+* main - check the code.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 int r;
@@ -14,7 +14,7 @@ _putchar(r + '0');
 r = _islower('o');
 _putchar(r + '0');
 r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+_putchar(r + '0');
+_putchar('\n');
+return (0);
 }
