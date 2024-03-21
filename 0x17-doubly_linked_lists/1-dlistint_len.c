b2ed93ea51d07353229bd6b32f16b9ff64fc6f7f
joshchif3 <pantherchif3@gmail.com>
2024-03-21 09:04:10 -0700

    0.2 ile

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in a linked dlistint_t list.
- * @h: pointer to the list.
- * Return: number of nodes.
+ * dlistint_len -
+ * @h:
+ * Return: num of nodes.
  **/
 size_t dlistint_len(const dlistint_t *h)
 {
